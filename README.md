6b4092b924c8d3f3dcb9c174f97d2e29c38083f2
akshat.shah <akshatshah04@gmail.com>
2022-11-09 15:09:48 +0530

    first

---
+++ README.md
@@ -0,0 +1,8 @@
+## Demo
+#### Here is a working live demo :  https://cryptotrack-cms.openxcell.dev/
+
+## Built with 
+
+- [React JS](https://reactjs.org/)
+- [Material UI](https://v4.mui.com/)
+- [Chart JS](https://reactchartjs.github.io/react-chartjs-2/#/)
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCRqfFK5kctAfmtMFTor3x1VYVe-MlZVMw",
+  authDomain: "cryptotracker-4fe66.firebaseapp.com",
+  projectId: "cryptotracker-4fe66",
+  storageBucket: "cryptotracker-4fe66.appspot.com",
+  messagingSenderId: "1058937696096",
+  appId: "1:1058937696096:web:75e37eca6a737cd9223805",
+  measurementId: "G-EH0BBEBXXD"
+};
+
+export default firebaseConfig;
